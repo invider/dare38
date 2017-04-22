339d9558e1811642799378ae51afd4f0a1d571a0
ayakushko <ayakushko@intropro.com>
2017-04-22 20:17:21 +0300

    [=] tmp commit

--- js/planet.js
+++ js/planet.js
@@ -15,6 +15,18 @@ var _titleBuilder = function (type) {
             return new Player()
     }
 };
+var centerField = function(field, w, h){
+    var maxX = 0;
+    for (var y = 0; y < field.length; i++){
+        maxX =Math.max(maxX, field[y].length);
+    }
+    if (maxX > w){
+        throw new Error("Error, width is to big, max is:" + w);
+    }
+    if (maxY > h){
+        throw new Error("Error, height is to big, max is:" + h);
+    }
+};
 
 var PlanetProto = function () {
     var my = this;
