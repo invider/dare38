b7d107ea3108deb562769d461e4bd7b76fa96a51
ayakushko <ayakushko@intropro.com>
2017-04-22 21:06:07 +0300

    [-] unbreakables are red now

--- js/titles/UnbreakableWall.js
+++ js/titles/UnbreakableWall.js
@@ -6,6 +6,12 @@ var UnbreakableWall = function () {
     this.stroke = function(){
         console.log("Not so fast!!!");
     }
+    this.render = function (ctx, scene) {
+        ctx.beginPath();
+        ctx.fillStyle = "red";
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.fill();
+    };
 };
 
 Util.extend(UnbreakableWall, Wall);
