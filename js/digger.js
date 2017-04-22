224f166fcb37f4b794535a7c70152209162a6ce4
Borys Sheludchenko <Borys_Sheludchenko1@epam.com>
2017-04-22 17:32:44 +0300

    digger

--- js/digger.js
+++ js/digger.js
@@ -1,11 +1,11 @@
 var Digger = function(x, y) {
-	var coord = Utils.point(x, y)
+	var p = Utils.point(x, y)
 	return {
 		evolve : function(delta) {
-			Utils.fall(point, delta);
-			Utils.boundY(point, 0, 200);
+			Utils.fall(p, delta);
+			Utils.boundY(p, 0, 200);
 		},
 		render : function(ctx) {
-		}
+		},
 	}
 }
