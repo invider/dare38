9a1372915a60501ab36ad42091f431154563b2f1
ayakushko <ayakushko@intropro.com>
2017-04-22 17:40:29 +0300

    [+] added basic planet

--- js/planet.js
+++ js/planet.js
@@ -2,24 +2,74 @@
  * Created by shaddy on 22.04.17.
  */
 
-var PlanetProto = function() {
-    this.x = 0;
-    this.y = 0;
-    this.children = [];
-    this.init = function(parentNode, scene){
-        this.parentNode = parentNode;
-        this.scene = scene;
-    };
-    this.evolve=function(delta, scene) {
-        //none done here
-
-    };
-
-    this.render = function(ctx, scene) {
-        //planet drawing
-        //ctx.strokeStyle="red";
-        //ctx.rect(this.x,this.y,40,40);
-        //ctx.stroke();
-
+var _titleBuilder = function (type) {
+    switch (type) {
+        case "X":
+            return new Wall();
+        case " ":
+            return new EmptySpace();
     }
 };
+
+var PlanetProto = function () {
+    var field = [];
+    for (var i = 1; i < arguments.lenght; i++) {
+        var chunk = arguments[i].split("\n")
+            .map(function (s) {
+                return s.trim();
+            });
+
+        field = field.concat(chunk);
+    }
+
+    this.x = 0;
+    this.y = 0;
+    this.xSize = field[0].length;
+    this.ySize = field.length;
+    this.children = [];
+    for (var y = 0; y < this.ySize; y++) {
+        var row = [];
+        var txtRow = field[y];
+        this.children.push(row);
+        for (var x = 0; x < this.xSize; x++) {
+            row.push(_titleBuilder(txtRow.charAt(x)));
+        }
+    }
+    this.getElement = function(x, y){
+        return this.children[x][y];
+    };
+    this.init = function (parentNode, scene) {
+        Util.initChildren(this, parentNode, scene);
+    };
+
+    this.evolve = function (delta, scene) {
+        //none done here
+        for (var y = 0; y < this.ySize; y++) {
+            for (var x = 0; x < this.xSize; x++) {
+
+            }
+        }
+        Util.evolveChildren(this, delta, scene);
+    };
+
+    this.render = function (ctx, scene) {
+        Util.renderChildren(this, delta, scene);
+    }
+};
+
+/**
+ * Planet initializers:
+ * X: wall
+ *
+ * @type {PlanetProto}
+ */
+var _Planet = new PlanetProto(
+    "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    "X                                                         X",
+    "X                                                         X",
+    "X                                                         X",
+    "X                                                         X",
+    "X                                                         X",
+    "X                                                         X",
+    "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+);
--- js/util.js
+++ js/util.js
@@ -2,6 +2,11 @@
  * Created by shaddy on 22.04.17.
  */
 var Util = {
+    initChildren: function(obj, parent, scene){
+        for (var i=0; i < obj.length; i++){
+            obj[i].init.call(ctx, parent, scene);
+        }
+    },
     renderChildren: function(obj, ctx, scene){
         for (var i=0; i < obj.length; i++){
             obj[i].render.call(ctx, scene);
