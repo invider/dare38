f9c5aa11e591bb9c7e2fec3808f1257ac3513a3e
ayakushko <ayakushko@intropro.com>
2017-04-22 20:25:33 +0300

    [+] added utility methods

--- js/planet.js
+++ js/planet.js
@@ -15,16 +15,33 @@ var _titleBuilder = function (type) {
             return new Player()
     }
 };
-var centerField = function(field, w, h){
-    var maxX = 0;
-    for (var y = 0; y < field.length; i++){
-        maxX =Math.max(maxX, field[y].length);
-    }
-    if (maxX > w){
-        throw new Error("Error, width is to big, max is:" + w);
-    }
-    if (maxY > h){
-        throw new Error("Error, height is to big, max is:" + h);
+PlanetUtil = {
+    genRow:function(w){
+        var line = "";
+        for (var i=0; i < w; i++){
+            line += " "
+        }
+        return line;
+    },
+    genRows:function(w, h){
+        var lines = [];
+        for (var i=0; i < h; i++){
+            lines.push(this.genRow(w))
+        }
+        return lines;
+    },
+    centerField: function(field, w, h) {
+        var maxX = 0;
+        for (var y = 0; y < field.length; i++) {
+            maxX = Math.max(maxX, field[y].length);
+        }
+        if (maxX > w) {
+            throw new Error("Error, width is to big, max is:" + w);
+        }
+        if (maxY > h) {
+            throw new Error("Error, height is to big, max is:" + h);
+        }
+        var elementsToAddX = (w - maxX);
     }
 };
 
@@ -60,7 +77,7 @@ var PlanetProto = function () {
      * @returns {Entity}
      */
     this.getElement = function(x, y){
-        return this.children[Math.ceil(y)][Math.ceil(x)];
+        return this.children[Math.floor(y)][Math.floor(x)];
     };
 
     this.removeNode = function(node){
