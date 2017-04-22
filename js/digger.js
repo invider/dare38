1a5e10865d5ea5c89a63885a2e06570d47b2bbed
Borys Sheludchenko <Borys_Sheludchenko1@epam.com>
2017-04-22 17:36:13 +0300

    digger

--- js/digger.js
+++ js/digger.js
@@ -1,11 +1,15 @@
 var Digger = function(x, y) {
+	
 	var p = Utils.point(x, y)
+	
 	return {
 		evolve : function(delta) {
 			Util.fall(p, delta);
 			Util.boundY(p, 0, 200);
 		},
+		
 		render : function(ctx) {
+			//ctx.
 		},
 	}
 }
--- js/util.js
+++ js/util.js
@@ -7,11 +7,13 @@ var Util = {
             obj[i].render.call(ctx, scene);
         }
     },
+    
     evolveChildren: function(obj, delta, scene){
         for (var i=0; i < obj.length; i++){
             obj[i].evolve.call(delta, scene);
         }
     },
+    
     point: function(x0, y0, v0) {
     	return {
     		x: x0 || 0,
@@ -19,10 +21,12 @@ var Util = {
     		v: v0 || 0
     	}
     },
+    
     fall: function(point, delta) {
     	point.y += point.v * delta;
     	point.v += 9.8 * delta;
     },
+    
     boundX: function(point, a, b) {
 		if(point.x < Math.min(a, b)) {
 			point.x = Math.min(a, b);
@@ -30,6 +34,7 @@ var Util = {
 			point.x = Math.max(a, b);
 		}
     },
+    
     boundY: function(point, a, b) {
 		if(point.y < Math.min(a, b)) {
 			point.y = Math.min(a, b);
