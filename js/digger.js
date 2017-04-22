4cc94f3d8b08639cbe6b307285e8160784220c8d
Borys Sheludchenko <Borys_Sheludchenko1@epam.com>
2017-04-22 18:12:52 +0300

    digger

--- js/digger.js
+++ js/digger.js
@@ -1,16 +1,17 @@
 var Digger = function(x, y) {
 	
 	var p = Util.point(x, y)
+	var w=10;
 	
 	return {
 		evolve : function(delta, scene) {
 			Util.fall(p, delta);
-			Util.boundY(p, 0, 300);//scene.height);
+			Util.boundY(p, 0, scene.height-w);
 		},
 		
 		render : function(ctx) {
 			ctx.beginPath()
-			ctx.rect(p.x-5, p.y-5, 10, 10)
+			ctx.rect(p.x-w/2, p.y-w/2, w,w)
 			ctx.fillStyle = 'green'
 			ctx.fill()
 		},
