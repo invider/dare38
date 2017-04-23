38008fd2ca0c2fff3792f3ae5c9f23cc39e27bfe
Borys Sheludchenko <Borys_Sheludchenko1@epam.com>
2017-04-23 16:39:17 +0300

    physics

--- js/Physics.js
+++ js/Physics.js
@@ -65,11 +65,13 @@ Physics.prototype.checkConstraints = function(element, delta){
 };
 
 Physics.prototype.evolve = function(element, delta){
+	var vx0 = element.horzVelocity; 
+	var vy0 = element.velocity; 
     element.horzVelocity += element.horzAcceleration * delta;
     element.velocity += (this.gravity - element.acceleration) * delta;
     this.checkConstraints(element, delta);
-    element.y += element.velocity * delta;
-    element.x += element.horzVelocity * delta;
+    element.y += 0.5 * (vy0 + element.velocity) * delta;
+    element.x += 0.5 * (vx0 + element.horzVelocity) * delta;
 };
 
 
