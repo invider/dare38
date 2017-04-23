2841d60c1cb5c8e292f008d7a4f8cc9a1979ea4a
Borys Sheludchenko <Borys_Sheludchenko1@epam.com>
2017-04-23 18:32:39 +0300

    move aoud unbreakable walls

--- js/activeElements/digger.js
+++ js/activeElements/digger.js
@@ -12,20 +12,24 @@ Digger.prototype.evolve = function(delta, scene) {
 	var killWall = function(wall){
 		wall.stroke(delta * 100);
 	};
+	
 	var location = this;
 	var dropped = function(wall) {
 		var p = scene.root.planet.getSpawnPoint();
 		var dx = p.x - location.x;
 		var dy = p.y - location.y;
 		var s = Math.sign(dx);
-		var sideWall = scene.root.planet.getElement(location.x + s, location.y);
-		if((Math.abs(dx) > Math.abs(dy)) && !(sideWall instanceof UnbreakableWall) || wall instanceof UnbreakableWall) {
+		var headWall = scene.root.planet.getElement(location.x + s, location.y);
+		var backWall = scene.root.planet.getElement(location.x - s, location.y);
+		if((Math.abs(dx) > Math.abs(dy)) && !(headWall instanceof UnbreakableWall) || wall instanceof UnbreakableWall) {
+			s = headWall instanceof UnbreakableWall ? -s : s;
 			scene.physics.accelerateToMaxHorzSpeed(location, 2 * s, 2);
 		} else {
 			location.horzAcceleration = 0;
 			killWall(wall);
 		}
 	}
+	
 	scene.physics.clearEvents();
 	scene.physics.onWall(dropped);
 	scene.physics.onHorzWall(killWall);
