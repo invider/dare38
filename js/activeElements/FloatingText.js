e45ef0ec9fa893e035a864c8d0cfa1c7029215ea
Igor Khotin <khotin@gmx.com>
2017-04-24 20:29:21 +0300

    added shaddows to floating text

--- js/activeElements/FloatingText.js
+++ js/activeElements/FloatingText.js
@@ -6,6 +6,10 @@ var FloatingText = function(x, y, label, color, font, ancor) {
     this.color = color? color : "#FFFFFF"
     this.font = font? font : '24px alien'
     this.ancor = ancor? ancor: 'absolute'
+    this.sdx = 4
+    this.sdy = 4
+    this.shaddow = true
+    this.shaddowColor = "#000000"
 }
 
 FloatingText.prototype.init = function(parent, scene) {
@@ -15,18 +19,30 @@ FloatingText.prototype.evolve = function(delta, scene) {
 }
 
 FloatingText.prototype.render = function(ctx, scene) {
-    ctx.fillStyle = this.color
     ctx.font = this.font
 
     switch(this.ancor) {
     case 'absolute':
         ctx.textBaseline = 'top'
+
+        if (this.shaddow) {
+            ctx.fillStyle = this.shaddowColor
+            ctx.fillText(this.label, this.x + this.sdx, this.y + this.sdy);
+        }
+        ctx.fillStyle = this.color
         ctx.fillText(this.label, this.x, this.y);
         break;
 
     case 'center':
         var thw = ctx.measureText(this.label).width/2
         ctx.textBaseline = 'middle'
+
+        if (this.shaddow) {
+            ctx.fillStyle = this.shaddowColor
+            ctx.fillText(this.label, scene.screenWidth/2 - thw + this.x + this.sdx,
+                scene.screenHeight/2 + this.y + this.sdy);
+        }
+        ctx.fillStyle = this.color
         ctx.fillText(this.label, scene.screenWidth/2 - thw + this.x,
             scene.screenHeight/2 + this.y);
         break;
