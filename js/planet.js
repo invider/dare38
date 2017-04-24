f60eb7c1943c1b01f50f9243c1556ede6cfcd2ac
Igor Khotin <khotin@gmx.com>
2017-04-24 17:39:13 +0300

    layers added

--- js/planet.js
+++ js/planet.js
@@ -156,7 +156,8 @@ var PlanetProto = function () {
             }
         });
         if (!retVal){
-            throw new Error("Could not find player spawn!!!");
+            // probably game over
+            //throw new Error("Could not find player spawn!!!");
         }
         return retVal;
     };
